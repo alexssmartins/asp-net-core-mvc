b9c4a5a39da3550e84298ae1bf5ad59305f58960
Alex Martins <alexssmartins@gmail.com>
2019-02-11 14:48:30 -0200

    SellersController

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellersController(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
